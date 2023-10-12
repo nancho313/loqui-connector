b16ab0ad913c0172b0d9e4c259c3d749e073ae94
Hernan Dario Jimenez <nancho313@hotmail.com>
2023-10-12 15:06:14 -0500

    initial version

---
+++ code/connector/boot/src/main/java/com/nancho313/loqui/connector/ConnectorApplication.java
@@ -0,0 +1,13 @@
+package com.nancho313.loqui.connector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectorApplication.class, args);
+	}
+
+}
---
+++ code/connector/infrastructure/src/main/java/com/nancho313/loqui/connector/App.java
@@ -0,0 +1,13 @@
+package com.nancho313.loqui.connector;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
